58651d88fd15ce67c2542373243e885dd22419d8
nathanael.ries <1423199+nathanaelries@users.noreply.github.com>
2022-04-20 08:35:52 -0500

    Update Caffeine.ps1

--- Caffeine.ps1
+++ Caffeine.ps1
@@ -1,6 +1,1 @@
-$wsh = New-Object -ComObject Wscript.Shell
-WHILE($TRUE){
-    $wsh.SendKeys("{SCROLLLOCK}")
-    $wsh.SendKeys("{SCROLLLOCK}")
-    START-SLEEP -Seconds 200
-}
+$wsh = New-Object -ComObject Wscript.Shell; WHILE($TRUE){$wsh.SendKeys("{SCROLLLOCK}");$wsh.SendKeys("{SCROLLLOCK}");START-SLEEP -Seconds (Get-Random -Minimum 45.1 -Maximum 110.99)}
